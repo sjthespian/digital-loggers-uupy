e17f1d4828738d6fd3131fc63d5db4a3ee068ef5
Daniel Rich <drich@employees.org>
2022-05-07 20:48:50 -0700

    Initial work at adding json support

--- UU.py
+++ UU.py
@@ -3,8 +3,12 @@
 # Port of Digital Logger UU to python for use with Home Assistant
 # https://www.digital-loggers.com/cex.html
 #
+# Daniel Rich <drich@employees.org>
+#
 
 import sys
+import argparse
+import json
 import re
 import requests
 
@@ -13,22 +17,15 @@ norefresh = 0
 # norefresh=1
 # to enable status caching
 
-version = "4.5-py (c) DLI 2016"
+version = "4.5-py (c) DLI 2016 Additional code (c) Daniel Rich 2022"
 type = "lpc"
 prompt = "UU> "
 response = None  # Global for response from requests
-
-print(f"UserUtil {version}\n", file=sys.stderr)
-
-
-def usage(msg=None):
-    if msg:
-        print(msg, file=sys.stderr)
-    print(
-        "Usage: UU <Host>[:port] <login:password> <[n]{on|off|pulse|status|power|name}|runNNN|interact> ...",
-        file=sys.stderr,
-    )
-    sys.exit(1)
+base = ""  # Base URL with username and password
+output_json = False  # True for JSON output
+debug = False  # True for debug output
+epc = None
+outputdata = {}  # Data to output
 
 
 def RelLink(args):
@@ -62,6 +59,7 @@ def RelLink(args):
 def cmd(arg=None):
     global response
     global type
+    global args
 
     arg = arg.lower()
     arg = re.sub("(^[^1-8])", r"a\1", arg)
@@ -92,6 +90,7 @@ def cmd(arg=None):
 
     m = re.search("^([1-8a])status$", arg)
     if m:
+        outputdata.setdefault("outlets", {})
         n = m.group(1)
         if n != "a":
             n = int(n)
@@ -121,19 +120,19 @@ def cmd(arg=None):
             lock = int(m.group(2), 16)
             for i in range(1, 9):
                 if i == n or n == "a":
-                    print(i, end="")
-                    print(" ON" if state & (1 << (i - 1)) else " OFF", end="")
-                    print(" LOCKED" if lock & (1 << (i - 1)) else "")
+                    statestr = " ON" if state & (1 << (i - 1)) else " OFF"
+                    lockstr = " LOCKED" if lock & (1 << (i - 1)) else ""
+                    outputdata["outlets"].setdefault("status", {})
+                    outputdata["outlets"]["status"][i] = f"{statestr}{lockstr}"
         else:
             for m in re.findall(
                 "<td.*?>([1-8])<\/td>\s*<td>(.*?)<\/td>[^\/]*?\W(ON|OFF)\W",
                 flags=re.IGNORECASE | re.MULTILINE,
             ):
                 if m[0] == n or n == "a":
-                    if m[2] == "ON":
-                        print(f"{m[0]} ON")
-                    else:
-                        print(f"{m[0]} OFF")
+                    statestr = " ON" if m[2] == "ON" else " OFF"
+                    outputdata["outlets"].setdefault("status", {})
+                    outputdata["outlets"]["status"][m[0]] = f"{statestr}"
 
         return True
 
@@ -162,7 +161,8 @@ def cmd(arg=None):
                     '<th bgcolor="#DDDDFF" align=left>\s+Controller:\s+([^\<]*)',
                     content,
                 )
-                print(f'Controller: "{m.group(1).strip()}"')
+                outputdata["outlets"].setdefault("name", {})
+                outputdata["outlets"]["name"][0] = m.group(1).strip()
 
         for m in re.findall(
             "<td.*?>([1-8])<\/td>\s*<td>(.*?)<\/td>[^\/]*?\W(ON|OFF)\W",
@@ -170,7 +170,8 @@ def cmd(arg=None):
             flags=re.IGNORECASE | re.MULTILINE,
         ):
             if m[0] == n or n == "a":
-                print(f'{m[0]}: "{m[1]}"')
+                outputdata["outlets"].setdefault("name", {})
+                outputdata["outlets"]["name"][m[0]] = m[1]
         return True
 
     m = re.search("^([1-8a])power$", arg)
@@ -190,7 +191,9 @@ def cmd(arg=None):
         content = response.content.decode("utf-8")
 
         if m:
+            outputdata["power"] = {}
             if n in ["1", "2", "3", "4", "a"]:
+                outputdata["power"]["A"] = {}
                 m = re.search(
                     "<!--\s+RAW\s+VA=(\d+)\s+CA=(\d+)\s+VAH=(\S+)\s+CAH=(\S+)\s+(?:WHA100=(\S+)\s+)?-->",
                     content,
@@ -199,8 +202,11 @@ def cmd(arg=None):
                 vah = f"{m.group(3)}V" if float(m.group(3)) > 0 else "n/a"
                 cah = f"{m.group(4)}A" if float(m.group(4)) > 0 else "n/a"
                 wha = f"{float(m.group(5))/10.0}kWh" if float(m.group(5)) > 0 else "n/a"
-                print(f"Bus A: V={vah} I={cah} W={wha}")
+                outputdata["power"]["A"]["voltage"] = vah
+                outputdata["power"]["A"]["amperage"] = cah
+                outputdata["power"]["A"]["wattage"] = wha
             if n in ["5", "6", "7", "8", "a"]:
+                outputdata["power"]["B"] = {}
                 m = re.search(
                     "<!--\s+RAW\s+VB=(\d+)\s+CB=(\d+)\s+VBH=(\S+)\s+CBH=(\S+)\s+(?:WHB100=(\S+)\s+)?-->",
                     content,
@@ -209,7 +215,9 @@ def cmd(arg=None):
                 vbh = f"{m.group(3)}V" if float(m.group(3)) > 0 else "n/a"
                 cbh = f"{m.group(4)}A" if float(m.group(4)) > 0 else "n/a"
                 whb = f"{float(m.group(5))/10.0}kWh" if float(m.group(5)) > 0 else "n/a"
-                print(f"Bus A: V={vbh} I={cbh} W={whb}")
+                outputdata["power"]["B"]["voltage"] = vbh
+                outputdata["power"]["B"]["amperage"] = cbh
+                outputdata["power"]["B"]["wattage"] = whb
 
         return True
 
@@ -224,30 +232,93 @@ def cmd(arg=None):
     return False
 
 
-if len(sys.argv) <= 1:
-    usage()
-(epc, auth) = sys.argv[1:3]
-base = f"http://{auth}@{epc}/"
+def tojson(data):
+    print(json.dumps(outputdata))
 
-for arg in sys.argv[3:]:
-    arg = arg.lower()
 
-    if arg != "interact":
-        if not cmd(arg):
-            usage(f"ERROR: unknown command {arg}")
-    else:
-        print(prompt, end="", flush=True)
-        for line in sys.stdin:
-            line = line.strip()
-            if line in ["?", "help"]:
-                print(
-                    "Commands: {?|help} | [n]{on|off|pulse|status|power|name} | runNNN | quit"
-                )
-            elif line == "quit":
-                break
-            elif cmd(line):
-                print("\t[OK]")
-            else:
-                print("\t[ERROR]")
+def totext(data):
+    if "power" in data:
+        for bus in data["power"]:
+            print(
+                f"Bus {bus}: V={data['power'][bus]['voltage']} I={data['power'][bus]['amperage']} W={data['power'][bus]['wattage']}"
+            )
+    if "outlets" in data:
+        if "name" in data["outlets"]:
+            for k, v in data["outlets"]["name"].items():
+                print(f"{('Controller' if k == 0 else k)}: \"{v}\"")
+        if "status" in data["outlets"]:
+            for k, v in data["outlets"]["status"].items():
+                print(f"{k}{v}")
+
+
+def main():
+    global base
+
+    # Parse command line
+    parser = argparse.ArgumentParser(
+        description="Communicate with Digital Loggers PDUs",
+        usage="%(prog)s [--json] [--debug] <Host>[:port] <login:password> <[n]{on|off|pulse|status|power|name}|runNNN|interact> ...",
+    )
+    parser.add_argument(
+        "--json",
+        action="store_true",
+        default=False,
+        help="Format output in JSON",
+    )
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        default=False,
+        help="enable debugging output",
+    )
+    (args, args_remaining) = parser.parse_known_args()
+
+    output_json = args.json
+    debug = args.debug
+
+    # Parse UU.pl style args
+    if len(args_remaining) == 0:
+        parser.print_usage()
+        sys.exit(1)
+    (epc, auth) = args_remaining[0:2]
+    args_remaining.pop(0)  # Removed used args
+    args_remaining.pop(0)
+    base = f"http://{auth}@{epc}/"
+
+    if not output_json:
+        print(f"UserUtil {version}\n", file=sys.stderr)
+
+    for arg in args_remaining:
+        arg = arg.lower()
+
+        if arg != "interact":
+            if not cmd(arg):
+                print(f"ERROR: unknown command {arg}")
+                parser.print_usage()
+                sys.exit(1)
+
+        else:
             print(prompt, end="", flush=True)
-        print("")
+            for line in sys.stdin:
+                line = line.strip()
+                if line in ["?", "help"]:
+                    print(
+                        "Commands: {?|help} | [n]{on|off|pulse|status|power|name} | runNNN | quit"
+                    )
+                elif line == "quit":
+                    break
+                elif cmd(line):
+                    print("\t[OK]")
+                else:
+                    print("\t[ERROR]")
+                print(prompt, end="", flush=True)
+            print("")
+
+    if output_json:
+        tojson(outputdata)
+    else:
+        totext(outputdata)
+
+
+if __name__ == "__main__":
+    main()
